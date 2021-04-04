e76c57246e6e9fa07fb20653cc6b013b4010a589
naumanxds <nauman.nasir919@gmail.com>
2021-04-05 00:13:19 +0500

    make relation of roles and users

---
+++ database/migrations/2021_03_29_140826_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('role_id')->nullable();
+
+            $table->foreign('role_id', 'users_n_roles_fk')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role_id');
+
+            $table->$table->dropForeign('users_n_roles_fk');
+        });
+    }
+}
